940889f2ee7c07584cc6f57dc96847b888a55ca5
vlevy <github@levysr.us>
2015-05-25 00:39:46 -0400

    First commit of the loader for the Million Song Dataset
    First commit of the loader for the Million Song Dataset. Has been tested
    with the Million Song Subset.

---
+++ load_million_song/src/load_million_song.py
@@ -0,0 +1,98 @@
+
+import os
+import h5py
+import re
+from time import time
+from collections import namedtuple
+
+song_record = namedtuple("song_record", "id  artist  album  title  pitches")
+
+def iterate_folder_songs(root_path, filename_re):
+    """Iterate over a collection of HDF5 database files, each one containing
+    structured data, presumably a song. 
+    Yields the full HDF5 record from the file.
+    Arguments:
+        root_path -- File path to the root of the file collection.
+        filename_re -- Regular expression to match HDF5 files
+    """
+    # Open and collect each file in the database
+    for dirpath, dirnames, filenames in os.walk(root_path):
+        # Parse each file in the current directory
+        for filename in filenames:
+            # Sanity check on file name
+            if filename_re and not re.search(filename_re, filename):
+                print "Warning: Skipping non-matching file name {0}".format(filename)
+                continue
+
+            # Open an individual song file
+            filepath = os.path.join(dirpath, filename)
+            with h5py.File(filepath, 'r') as song_file:
+                yield song_file
+
+def iterate_folder_songs_extracted(root_path, filename_re):
+    """Iterate over a collection of HDF5 database files, each one containing
+    structured data, presumably a song. 
+    Yields a processed record of song data.
+    Arguments:
+        root_path -- File path to the root of the file collection.
+        filename_re -- Regular expression to match HDF5 files
+    """
+    for song in iterate_folder_songs(root_path, filename_re):
+        
+        # Extract the important data from the full song record
+        metadata = song["metadata"]["songs"][0]
+        id = metadata[17]
+        artist = metadata[9]
+        album = metadata[14]
+        title = metadata[18]
+
+        analysis = song["analysis"]
+        pitches = analysis["segments_pitches"]
+
+        # Combine into a song record
+        song_rec = song_record(id, artist, album, title, pitches)
+        yield song_rec
+
+def print_pitches(pitches):
+    """Function to show how to interpret the pitches (chroma) member returned
+    from the database
+    """
+    num_pitch_sets = pitches.shape[0]
+    print "num_pitch_sets:", num_pitch_sets,
+
+    num_pitches_per_set = pitches.shape[1]
+    print "num_pitches_per_set:", num_pitches_per_set,
+
+    first_set_of_12_pitches = pitches[0]
+    print "first_set_of_12_pitches:",first_set_of_12_pitches,
+    print "Third pitch:",first_set_of_12_pitches[2],
+
+if __name__ == '__main__':
+    """Main program, iterates the whole database
+    To run this test, download the Echonest Million Song Subset, available at:
+        http://labrosa.ee.columbia.edu/millionsong/pages/getting-dataset#subset
+    which links directly to the file:
+        http://static.echonest.com/millionsongsubset_full.tar.gz
+    Extract the tarball to a directory and set the root_path variable below
+    to the data root directory.
+    """
+    root_path = "MillionSongSubset/data"
+    filename_re = "^[A-Z]{7}[0-9,A-F]{11}\.h5$" # Example: TRBIJIA128F425F57D.h5
+    time_start = time()
+    for loop_nr, song_rec in enumerate(
+                iterate_folder_songs_extracted(root_path, filename_re)):
+
+        if loop_nr == 0:
+            # Debugging
+            print song_rec,
+            print_pitches(song_rec.pitches)
+            print
+
+        # Progress report        
+        if ( (loop_nr + 1) % 1000) == 0:
+            print "{0} songs read in {1:.1f} seconds" \
+                .format(loop_nr + 1, time() - time_start)
+
+    end_time = time() - time_start
+    print "Total: {0} songs read in {1:.1f} seconds".format(loop_nr + 1, end_time)
+    
